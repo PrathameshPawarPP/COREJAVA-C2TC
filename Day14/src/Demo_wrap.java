ff6e8745d96193deebcbe027459d6a92e127256b
pkprathamesh07 <pkprathamesh07@Prathameshs-Gaming-lappy.mshome.net>
2022-03-21 16:48:51 +0530

    d14

---
+++ Day14/src/Demo_wrap.java
@@ -0,0 +1,13 @@
+public class Demo_wrap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=20;
+Integer a=i;
+Integer j=new Integer(10);
+System.out.println(+a);
+System.out.println(+j);
+System.out.println(+i);
+	}
+
+}
